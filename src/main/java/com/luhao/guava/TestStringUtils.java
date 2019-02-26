bc0b3a4b3e09e3c218033d2989712fc101b5b406
howroad <kknn2@vip.qq.com>
2019-02-26 17:39:16 +0800

    java学习的工程

---
+++ src/main/java/com/luhao/guava/TestStringUtils.java
@@ -0,0 +1,19 @@
+package com.luhao.guava;
+
+/**
+ * <p>Title: TestStringUtils.java</p>
+ *
+ * <p>Description: </p>
+ *
+ * <p>Company: 北京九恒星科技股份有限公司</p>
+ *
+ * @author luhao
+ * 
+ * @since：2019年2月26日 下午5:27:05
+ * 
+ */
+public class TestStringUtils {
+    public void test1() {
+        
+    }
+}
---
+++ src/main/java/com/luhao/java/TestJava.java
@@ -0,0 +1,24 @@
+package com.luhao.java;
+
+import org.junit.Before;
+import org.junit.Test;
+/**
+ * <p>Title: Test.java</p>
+ *
+ * <p>Description: </p>
+ *
+ * @author luhao
+ * 
+ * @since：2019年2月26日 下午5:25:51
+ * 
+ */
+public class TestJava {
+    @Before
+    public void before() {
+        
+    }
+    @Test
+    public void test() {
+        System.out.println("test!");
+    }
+}
