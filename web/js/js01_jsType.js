e7b00e010b0ac0d3c8dd9d2811e39b8f0a6902bc
howroad <kknn2@vip.qq.com>
2019-05-21 09:26:54 +0800

    js使用es5

--- web/js/js01_jsType.js
+++ web/js/js01_jsType.js
@@ -1,61 +1,61 @@
 $(function(){
     debugger;
-    let str1 = "";
-    let str2 = "str2";
+    var str1 = "";
+    var str2 = "str2";
 
-    let num1 = 1;
-    let num2 = Number.NaN;
-    let num3 = Number.MIN_VALUE;
-    let num41 = Number.NEGATIVE_INFINITY; //负无穷
-    let num42 = Number.POSITIVE_INFINITY; //正无穷
-    let num4 = Infinity;
-    let num5 = Number.MAX_VALUE;
-    let num6 = 0;
+    var num1 = 1;
+    var num2 = Number.NaN;
+    var num3 = Number.MIN_VALUE;
+    var num41 = Number.NEGATIVE_INFINITY; //负无穷
+    var num42 = Number.POSITIVE_INFINITY; //正无穷
+    var num4 = Infinity;
+    var num5 = Number.MAX_VALUE;
+    var num6 = 0;
 
-    let boo1 = false;
-    let boo2 = true;
+    var boo1 = false;
+    var boo2 = true;
 
-    let nul = null;
+    var nul = null;
 
-    let udf = undefined;
+    var udf = undefined;
 
-    let obj = new Object();
+    var obj = new Object();
 
 
     //所有类型转字符串
-    let toStr1 = String(num1);
-    let toStr3 = String(num2);
-    let toStr4 = String(num3);
-    let toStr5 = String(num4);
-    let toStr6 = String(num5);
-    let toStr7 = String(num6);
-    let toStr8 = String(boo1);
-    let toStr9 = String(boo2);
-    let toStr10 = String(nul);
-    let toStr11 = String(udf);
-    let toStr12 = String(obj);
+    var toStr1 = String(num1);
+    var toStr3 = String(num2);
+    var toStr4 = String(num3);
+    var toStr5 = String(num4);
+    var toStr6 = String(num5);
+    var toStr7 = String(num6);
+    var toStr8 = String(boo1);
+    var toStr9 = String(boo2);
+    var toStr10 = String(nul);
+    var toStr11 = String(udf);
+    var toStr12 = String(obj);
 
     //所有类型转数字
-    let toNum1 = Number(str1);
-    let toNum2 = Number(str2);
-    let toNum3 = Number(boo1);
-    let toNum4 = Number(boo2);
-    let toNum5 = Number(nul);
-    let toNum6 = Number(udf);
-    let toNum7 = Number(obj);
+    var toNum1 = Number(str1);
+    var toNum2 = Number(str2);
+    var toNum3 = Number(boo1);
+    var toNum4 = Number(boo2);
+    var toNum5 = Number(nul);
+    var toNum6 = Number(udf);
+    var toNum7 = Number(obj);
 
     //所有类型转布尔
-    let toBol1 = Boolean(str1);
-    let toBol2 = Boolean(str2);
-    let toBol4 = Boolean(num1);
-    let toBol5 = Boolean(num2);
-    let toBol6 = Boolean(num3);
-    let toBol7 = Boolean(num4);
-    let toBol8 = Boolean(num5);
-    let toBol9 = Boolean(num6);
-    let toBol10 = Boolean(nul);
-    let toBol11 = Boolean(udf);
-    let toBol12 = Boolean(obj);
+    var toBol1 = Boolean(str1);
+    var toBol2 = Boolean(str2);
+    var toBol4 = Boolean(num1);
+    var toBol5 = Boolean(num2);
+    var toBol6 = Boolean(num3);
+    var toBol7 = Boolean(num4);
+    var toBol8 = Boolean(num5);
+    var toBol9 = Boolean(num6);
+    var toBol10 = Boolean(nul);
+    var toBol11 = Boolean(udf);
+    var toBol12 = Boolean(obj);
 
     //所有类型转Obj
 
