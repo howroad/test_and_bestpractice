cbd51a67c8d927e72d1d4f758b8b516e2b4338f2
howroad <kknn2@vip.qq.com>
2020-04-07 13:35:18 +0800

    正则表达式备注

--- src/main/java/com/luhao/reg/TestCommont.java
+++ src/main/java/com/luhao/reg/TestCommont.java
@@ -13,7 +13,7 @@ import java.util.regex.Pattern;
  */
 public class TestCommont {
     public static void main(String[] args) {
-        String regex =  "(19|20)\\d\\d[- /.]#年，只允许是19或20开头的\n" +
+        String regex =  "(19|20)\\d\\d[- /.]#年，只允许是19或20开头的\n" + //#开头 回车结尾
                 "(0?[1-9]|1[012])[- /.]#月，只允许1-12\n" +
                 "(0?[1-9]|[12][0-9]|3[01])#日，只允许1到31";
         Pattern p = Pattern.compile(regex, Pattern.COMMENTS);
