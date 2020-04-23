c84e6a062c64ab58cab5e732c7f3a5f48d9a81ab
howroad <kknn2@vip.qq.com>
2020-04-23 20:37:27 +0800

    NIO NIO学习和记录2

---
+++ src/main/java/com/luhao/io/nio/TestChannel.java
@@ -0,0 +1,96 @@
+package com.luhao.io.nio;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SocketChannel;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * <p>Title: TestChannel.java</p>
+ * <p>Description: </p>
+ * <p>Company: 北京九恒星科技股份有限公司</p>
+ *
+ * @author luhao
+ * @since 2020-04-23 22:39
+ */
+public class TestChannel {
+
+    public static void main(String[] args) {
+        new Thread(TestChannel::server).start();
+        new Thread(TestChannel::client).start();
+    }
+
+
+    public static void client() {
+        ByteBuffer buffer = ByteBuffer.allocate(1024);
+        SocketChannel socketChannel = null;
+        try {
+            socketChannel = SocketChannel.open();
+
+            socketChannel.configureBlocking(false);
+            socketChannel.connect(new InetSocketAddress("127.0.0.1", 8080));
+            if (socketChannel.finishConnect()) {
+                int i = 0;
+                while (true) {
+                    TimeUnit.SECONDS.sleep(1);
+                    String info = "I'm " + i++ + "-th information from client";
+                    buffer.clear();
+                    buffer.put(info.getBytes());
+                    buffer.flip();
+                    while (buffer.hasRemaining()) {
+                        System.out.println(buffer);
+                        socketChannel.write(buffer);
+                    }
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (socketChannel != null) {
+                    socketChannel.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+    public static void server() {
+        ServerSocket serverSocket = null;
+        InputStream in = null;
+        try {
+            serverSocket = new ServerSocket(8080);
+            int recvMsgSize = 0;
+            byte[] recvBuf = new byte[1024];
+            while (true) {
+                Socket clntSocket = serverSocket.accept();
+                SocketAddress clientAddress = clntSocket.getRemoteSocketAddress();
+                System.out.println("Handling client at " + clientAddress);
+                in = clntSocket.getInputStream();
+                while ((recvMsgSize = in.read(recvBuf)) != -1) {
+                    byte[] temp = new byte[recvMsgSize];
+                    System.arraycopy(recvBuf, 0, temp, 0, recvMsgSize);
+                    System.out.println(new String(temp));
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (serverSocket != null) {
+                    serverSocket.close();
+                }
+                if (in != null) {
+                    in.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
